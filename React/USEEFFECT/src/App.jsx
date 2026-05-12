d6ee0830f6a87a300a2b5fa6d8deff219127db41
adarsh-rajput01 <adarsh.25b01010177@abes.ac.in>
2026-05-12 11:50:51 +0530

    Useffect

---
+++ React/USEEFFECT/src/App.jsx
@@ -0,0 +1,65 @@
+import './App.css';
+import React, { useState, useEffect } from 'react';
+
+
+const App = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: ''
+  });
+
+  useEffect(() => {
+    console.log("Form Data updated:", formData);
+  }, [formData]);
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert("Registration successful!");
+    console.log(formData);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="name"
+          placeholder="Enter your name"
+          value={formData.name}
+          onChange={handleChange}
+        />
+        <br /><br />
+
+        <input
+          type="email"
+          name="email"
+          placeholder="Enter your email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+        <br /><br />
+
+        <input
+          type="password"
+          name="password"
+          placeholder="Enter your password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+        <br /><br />
+
+        <button type="submit">Registration</button>
+      </form>
+    </div>
+  );
+};
+
+export default App;
